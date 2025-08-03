d45af9e319d31148157557a5d7e5be0e67965e04
amoljagtap17 <amoljagtap17@gmail.com>
2025-08-03 14:38:28 +0530

    feature/backend: add examples.http to test api endpoints

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -20,7 +20,7 @@ import { PostsModule } from './posts/posts.module';
         password: configService.get<string>('DB_PASSWORD', 'postgres'),
         database: configService.get<string>('DB_DATABASE', 'posts'),
         entities: [Post],
-        synchronize: false,
+        synchronize: true,
       }),
       inject: [ConfigService],
     }),
