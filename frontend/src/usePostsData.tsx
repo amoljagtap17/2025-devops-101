8c6e79375d37f44ca5e8e86668ffb8163e4a0dea
amoljagtap17 <amoljagtap17@gmail.com>
2025-08-09 21:24:01 +0530

    feature/k8s: add yaml files in k8s for frontend

--- frontend/src/usePostsData.tsx
+++ frontend/src/usePostsData.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 // const API_URL = import.meta.env.VITE_API_URL;
 
-const API_URL = "http://localhost:3000";
+const API_URL = "http://localhost:30001";
 
 export interface IPost {
   id: string;
